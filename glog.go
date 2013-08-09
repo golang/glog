f74c4c6b9610b9a36eeea83b7c18c9320b7fcc8d
Rob Pike <r@golang.org>
2013-08-09 13:23:55 +1000

    Add documentation about flushing and flags.

--- glog.go
+++ glog.go
@@ -32,6 +32,37 @@
 //
 //	glog.V(2).Infoln("Processed", nItems, "elements")
 //
+// Log output is buffered and written periodically using Flush. Programs
+// should call Flush before exiting to guarantee all log output is written.
+//
+// By default, all log statements write to files in a temporary directory.
+// This package provides several flags that modify this behavior.
+//
+//	-logtostderr=false
+//		Logs are written to standard error instead of to files.
+//	-alsologtostderr=false
+//		Logs are written to standard error as well as to files.
+//	-stderrthreshold=ERROR
+//		Log events at or above this severity are logged to standard
+//		error as well as to files.
+//
+//	Other flags provide aids to debugging.
+//
+//	-log_backtrace_at=""
+//		When set to a file and line number holding a logging statement,
+//		such as
+//			-log_backtrace_at=gopherflakes.go:234
+//		a stack trace will be written to the Info log whenever execution
+//		hits that statement. (Unlike with -vmodule, the ".go" must be
+//		present.)
+//	-v=0
+//		Enable V-leveled logging at the specified level.
+//	-vmodule=""
+//		The syntax of the argument is a comma-separate list of pattern=N,
+//		where pattern is a literal file name (minus the ".go" suffix) or
+//		"glob" pattern and N is a V level. For instance, -vmodule=gopher*=3
+//		sets the V level to 3 in all Go files whose names begin "gopher".
+//
 package glog
 
 import (
