30a6e468388451cd672fbad93a3eeea29e30c92d
Tobias Klauser <tklauser@distanz.ch>
2020-08-06 16:53:09 +0200

    Set GO111MODULE=off for go get golang.org/x/exp/cmd/apidiff
    Otherwise it will be added to go.mod as a dependency.

--- hack/verify-apidiff.sh
+++ hack/verify-apidiff.sh
@@ -65,7 +65,7 @@ fi
 if ! which apidiff > /dev/null; then
   echo "Installing golang.org/x/exp/cmd/apidiff..."
   pushd "${TMPDIR:-/tmp}" > /dev/null
-    go get golang.org/x/exp/cmd/apidiff
+    GO111MODULE=off go get golang.org/x/exp/cmd/apidiff
   popd > /dev/null
 fi
 
