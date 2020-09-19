d521fd2e1f4fba741c08db6a22ca37e76f69ad6f
Gregor Noczinski <gregor@noczinski.eu>
2020-09-19 19:58:31 +0200

    Forwards keysAndValues as an slice and not as a slice of slices

--- klog.go
+++ klog.go
@@ -759,7 +759,7 @@ func (l *loggingT) printWithFileLine(s severity, logr logr.Logger, file string, 
 // if loggr is specified, will call loggr.Error, otherwise output with logging module.
 func (l *loggingT) errorS(err error, loggr logr.Logger, msg string, keysAndValues ...interface{}) {
 	if loggr != nil {
-		loggr.Error(err, msg, keysAndValues)
+		loggr.Error(err, msg, keysAndValues...)
 		return
 	}
 	l.printS(err, msg, keysAndValues...)
@@ -768,7 +768,7 @@ func (l *loggingT) errorS(err error, loggr logr.Logger, msg string, keysAndValue
 // if loggr is specified, will call loggr.Info, otherwise output with logging module.
 func (l *loggingT) infoS(loggr logr.Logger, msg string, keysAndValues ...interface{}) {
 	if loggr != nil {
-		loggr.Info(msg, keysAndValues)
+		loggr.Info(msg, keysAndValues...)
 		return
 	}
 	l.printS(nil, msg, keysAndValues...)
